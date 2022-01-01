bddfd7a6737e6571568fb17b988e136de923b597
MargChatz <margaritaxatzouli@gmail.com>
2022-01-01 23:05:34 +0200

    implemented functionality of add data form
    validation not implemented(should do it in js)

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def home():
         country_name = request.form['countries']
         country_name = country_name.upper()
 
-        cursor = dbc.get_db().execute("select * from Data where country == ?", [country_name])
+        cursor = dbc.get_db().execute("select * from Data where country == ? and champion == 'true'", [country_name])
         results = cursor.fetchall()
         # print(results)
         # print(type(results))
@@ -34,6 +34,7 @@ def home():
         ld = None
        
         for result in results:
+
             td["country"] = result[0]
             td["LLS"] = result[1]
             td["DE"] = result[2]
@@ -88,7 +89,7 @@ def home():
 
 
     # Get country names from db
-    cursor = dbc.get_db().execute("select country from Data")
+    cursor = dbc.get_db().execute("select distinct(country) from Data")
     results = cursor.fetchall()
     # print(type(results))  # check what cursor returns (list of tuples)
     # print(results)
@@ -98,6 +99,7 @@ def home():
     # for country in results:
     #     country_name.append(country[0])
     print(country_names)
+    dbc.close_connection()
     return render_template("form.html", option_list=country_names)
 
 
@@ -106,6 +108,55 @@ def country():
     print("Country")
     print(request.method)
 
+
 @app.route("/addData",  methods=['GET','POST'])
 def addData():
+    
+    if request.method == "POST":
+        result = request.form.to_dict()
+        print (result)
+
+       
+        #result.items() list of tuples k for key and v for values
+        for k,v in result.items():
+            #if value is empty put none
+            if not v:
+                result[k] = "NULL"
+        
+        #Checkbox create false value
+        if "is_champion" not in result.keys():
+            result["is_champion"] = False
+
+        #print(result["is_champion"])
+
+        db = dbc.get_db()
+
+        insert_data_sql = "Insert into Data values(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13)" 
+        data = (result["country_input"],
+                result["LLS_input"],
+                result["DE_input"],
+                result["LA_input"],
+                result["ng_input"],
+                result["median_p_input"],
+                result["std_p_input"],
+                result["median_n_input"],
+                result["std_n_input"],
+                result["multi_input"],
+                result["comment_input"],
+                result["cite_input"],
+                result["is_champion"])  
+    
+        # insert_data_sql = "Insert into {} ({}) values({})" \
+        #     .format("Data",
+        #             "country", 
+        #             result["country_input"]
+        #             )
+        print(insert_data_sql)
+        cursor = db.execute(insert_data_sql, data)
+
+        
+        db.commit()
+            
+        dbc.close_connection()
+
     return render_template("addDataForm.html")
