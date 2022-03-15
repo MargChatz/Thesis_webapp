693c534f11bfa3a3010e8be2a1614aaf5db312e8
MargChatz <margaritaxatzouli@gmail.com>
2022-03-15 20:19:46 +0200

    Adds login page and changes code structure

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from math import e
-from os import EX_PROTOCOL, error
+from os import error
 from re import DEBUG
 from flask import (Flask, render_template, request, g, redirect, url_for,jsonify)
 import json
+import traceback
+import hashlib
 
 from mathcad import graph_values
 import database_controller as dbc
@@ -16,99 +18,52 @@ app.config["DEBUG"] = True
 @app.route("/", methods=['GET','POST'])
 @app.route("/home", methods=['GET','POST'])
 def home():
-    print(request.method)
 
     if request.method == "POST":
-        country_name = request.form['countries']
-        country_name = country_name.upper()
-
-        cursor = dbc.get_db().execute("select * from Data where country == ? and champion == 1", [country_name])
-        results = cursor.fetchall()
-        # print(results)
-        # print(type(results))
-        # print(results[0][0])
-        
         td = {}
         gd = {}
         ld = None
-       
-        for result in results:
-
-            td["country"] = result[0]
-            td["LLS"] = result[1]
-            td["DE"] = result[2]
-            td["LA"] = result[3]
-            td["percentage"] = result[4]
-
-            gd["median_current_p"] = result[5]
-            gd["std_current_p"] = result[6]
-            gd["median_current_n"] = result[7]
-            gd["std_current_n"] = result[8]
-
-            td["multiplicity"] = result[9]
-            td["comments"] = result[10]
-            td["literature"] = result[11]
-
         
-        j = json.dumps(td, indent=4)
-        with open("data.json", "w") as outfile:
-            outfile.write(j)
+        # Getting data for selected country
+        country_name = request.form['countries']
+        #country_name = country_name.upper()
+        result = dbc.get_champion_country_info(country_name)
+        # Creating two dictionaries out of the data we got from db
+        td, gd = get_champion_country_dict_info (result)
 
-        # Calculate graph values
-        gd["xs"], gd["graph_values_p"] = graph_values(gd["median_current_p"], gd["std_current_p"], 200)
-        gd["graph_values_n"] = graph_values(gd["median_current_n"], gd["std_current_n"], 200)[1]
-        
-        gd["csv_data_p"] = []
-        gd["csv_data_n"] = []
-        for i in range(len(gd["xs"])):
-            gd["csv_data_p"].append([gd["xs"][i], gd["graph_values_p"][i]])
-            gd["csv_data_n"].append([gd["xs"][i], gd["graph_values_n"][i]])
-
-        # print(gd["csv_data_p"])
-
-
-        # print(gd["xs"], gd["graph_values_p"])
-        # print(gd["graph_values_n"])
-        # print(gd["graph_values_p"])
-
-
-        country_name = "CIGRE STD"
-        cursor = dbc.get_db().execute("select * from Data where country == ?", [country_name])
-        results = cursor.fetchall()
-        for result in results:
-            gd["cigre_median_current_p"] = result[5]
-            gd["cigre_std_current_p"] = result[6]
-            gd["cigre_median_current_n"] = result[7]
-            gd["cigre_std_current_n"] = result[8]
+        # Getting data for CIGRE standard
+        country_name = "CIGRE"
+        result = dbc.get_champion_country_info(country_name)
+        # Adding CIGRE data to graph data dictionary
+        gd["cigre_median_current_p"] = result[5]
+        gd["cigre_std_current_p"] = result[6]
+        gd["cigre_median_current_n"] = result[7]
+        gd["cigre_std_current_n"] = result[8]
         gd["cigre_graph_values_p"] = graph_values(gd["cigre_median_current_p"], gd["cigre_std_current_p"], 200)[1]
         gd["cigre_graph_values_n"] = graph_values(gd["cigre_median_current_n"], gd["cigre_std_current_n"], 200)[1]
-        # print(gd["cigre_graph_values_p"])
-        # print(gd["cigre_graph_values_n"])
-        dbc.close_connection()
 
-        print(td)
+        dbc.close_connection()
         return render_template("index.html", table_data=td, graph_data=gd, literature_data=ld)
 
 
     # Get country names from db
-    cursor = dbc.get_db().execute("select distinct(country) from Data")
+    cursor = dbc.get_db().execute("SELECT distinct(country) FROM Data WHERE CHAMPION == 1")
     results = cursor.fetchall()
-    # print(type(results))  # check what cursor returns (list of tuples)
-    # print(results)
+
     country_names = [country[0] for country in results]
     # The above line is equal as the below block
     # country_name = []
     # for country in results:
     #     country_name.append(country[0])
-    print(country_names)
+
     dbc.close_connection()
     return render_template("form.html", option_list=country_names)
 
 
-@app.route("/country",  methods=['GET','POST'])
-def country():
-    print("Country")
-    print(request.method)
+# @app.route("/country",  methods=['GET','POST'])
+# def country():
+#     print("Country")
+#     print(request.method)
 
 
 @app.route("/addData",  methods=['GET','POST'])
@@ -117,9 +72,7 @@ def addData():
     if request.method == "POST":
         try:
             result = request.form.to_dict()
-            print (result)
 
-        
             #result.items() list of tuples k for key and v for values
             for k,v in result.items():
                 #if value is empty put none
@@ -132,10 +85,8 @@ def addData():
             else:
                 result["is_champion"] = 1
 
-            #print(result["is_champion"])
-
+            # Id of the new entry should be +1 from db's max id.
             db = dbc.get_db()
-            
             max_id_in_db = db.execute("SELECT max(ID) from Data").fetchall()[0][0]
             new_entry_id = max_id_in_db + 1
 
@@ -158,20 +109,75 @@ def addData():
             champion_id = dbc.get_champion_id(db,result["country_input"])
             if result["is_champion"] == 1 and champion_id != -1:
                 dbc.set_not_champion(db,champion_id)
-            print(insert_data_sql)
             
             cursor = db.execute(insert_data_sql, data)
 
-
+            # In order to store changes to the db, we commit.
             db.commit()
-                
             dbc.close_connection()
+
+            # result = 1 means everything went well.
             result = {"result": 1}
             return render_template("addDataForm.html", previous_form = result)
         except e:
             print(e)
+            print(traceback.format_exc())
+            # result = 0 means everything error.
             result = {"result": 0}
             return render_template("addDataForm.html",previous_form = result)
+
     result = {"result": -1}
     return render_template("addDataForm.html",previous_form = result)
 
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        
+        pass_to_bytes = str(request.form['password']).encode()
+        hash_result = hashlib.md5(pass_to_bytes)
+        pass_hex = hash_result.hexdigest()
+
+        access = dbc.check_if_user_allowed_to_add_data(dbc.get_db(), request.form['username'], pass_hex)
+        if not access:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect('addData')
+    return render_template('login.html', error=error)
+
+
+def get_champion_country_dict_info (result: list):
+    """
+    
+    """
+    
+    table_data = {}
+    table_data["country"] = result[0]
+    table_data["LLS"] = result[1]
+    table_data["DE"] = result[2]
+    table_data["LA"] = result[3]
+    table_data["percentage"] = result[4]
+
+    graph_data = {}
+    graph_data["median_current_p"] = result[5]
+    graph_data["std_current_p"] = result[6]
+    graph_data["median_current_n"] = result[7]
+    graph_data["std_current_n"] = result[8]
+
+    table_data["multiplicity"] = result[9]
+    table_data["comments"] = result[10]
+    table_data["literature"] = result[11]
+
+    # Calculate graph values
+    graph_data["xs"], graph_data["graph_values_p"] = graph_values(graph_data["median_current_p"], graph_data["std_current_p"], 200)
+    graph_data["graph_values_n"] = graph_values(graph_data["median_current_n"], graph_data["std_current_n"], 200)[1]
+    
+    graph_data["csv_data_p"] = []
+    graph_data["csv_data_n"] = []
+    for i in range(len(graph_data["xs"])):
+        graph_data["csv_data_p"].append([graph_data["xs"][i], graph_data["graph_values_p"][i]])
+        graph_data["csv_data_n"].append([graph_data["xs"][i], graph_data["graph_values_n"][i]])
+    
+    return table_data, graph_data
--- database_controller.py
+++ database_controller.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import g
 import traceback
 
-DATABASE = '/home/xhino/Desktop/marg_thesis/Thesis_webapp/database.db'
+DATABASE = 'C:\Users\30697\Desktop\Thesis\flask\lightining_app\Thesis_webapp\database.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -10,13 +10,10 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-#app.teardown_appcontext
 def close_connection():
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-
 
 def get_champion_id (db, country_name: str):
     '''If there is a champion country return the id, otherwise return NULL.'''
@@ -29,6 +26,17 @@ def get_champion_id (db, country_name: str):
             return result[13]
     return -1
 
+def get_champion_country_info (country_name: str):
+    '''Gets information regarding the selected country.
+    Returns only champion country information.
+    '''
+    db = get_db()
+    sql_query = "SELECT * FROM Data WHERE country == ? AND champion == 1"
+    cursor = db.execute(sql_query, (country_name,))
+    result = cursor.fetchone()
+    print(result)
+    return result
+
 def set_not_champion (db, id: int):
     '''Set champion values to 0.'''
     try:
@@ -37,3 +45,14 @@ def set_not_champion (db, id: int):
         db.commit()
     except :
         traceback.print_exc()
+
+def check_if_user_allowed_to_add_data (db, username: str, password: str):
+    '''Returns True/False based on whether the user and password match any admin pair in the db.'''
+
+    sql_query = "SELECT * FROM USER_CREDENTIALS WHERE USERNAME == ? AND PASSWORD == ?"
+    cursor = db.execute(sql_query, (username, password))
+    result = cursor.fetchone()
+    if result == None:
+        return False
+    else:
+        return True
